0e92ce22a46ad15809a1cd37f39182258944683d
Luke <55601762+skywa04885@users.noreply.github.com>
2024-04-18 13:55:54 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,15 @@
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
 rustup install stable
 rustup component add rustfmt
